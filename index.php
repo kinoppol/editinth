c17ba6eb46554e460a09942379267a5e6fed76ae
kinoppol <kinoppol@hotmail.com>
2021-11-24 23:23:21 +0700

    init

---
+++ index.php
@@ -0,0 +1,681 @@
+
+<!doctype html>
+<html lang="th">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="BackTrack Linux was 2006-2013. Now reborn as Kali Linux">
+
+  <title>EDIT-Educational information tools in thailand</title>
+  <link rel="shortcut icon" href="favicon.ico" />
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+  <style>
+  body{
+    font-family: 'Kanit', sans-serif;
+  }
+  :is(h1, h2, h3, h4, h5, h6) {
+    font-family: 'Kanit', sans-serif;
+}
+
+span {
+    font-family: 'Kanit', sans-serif;
+}
+  </style>
+  <style>
+  .select2-container--default .select2-selection--multiple .select2-selection__choice {
+    background-color: #3c8dbc;
+  }
+
+  .ui-autocomplete {
+    z-index: 5000;
+  }
+  .nav-tabs{
+    background-color: #009688 ;
+  }
+  /* .tab-content{
+      background-color: 5000;
+      color:#ffffcc;
+      padding:5px
+  } */
+  .nav-tabs > li > a{
+    border: medium none;
+  }
+  /* .nav-tabs > li > a:hover{
+    background-color: #303136 !important;
+      border: medium none;
+      border-radius: 0;
+      color:#fff;
+  } */
+
+  </style>
+  <style>
+html {
+}
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+*{
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+h1 {
+  font-weight: 400;
+  min-width:300px !important;
+}
+h2 {
+  color: #aaa;
+  font-weight:400;
+}
+
+a {
+  color: #d03540;
+  color: #fff;
+  text-decoration:none;
+  font-weight:400;
+}
+a:hover {
+  color: #fff;
+}
+
+body {
+  background-color: #000;
+  background-image:url(images/110411-black-blurred-background-vector.jpg);
+  background-size:cover;
+  /*background-size: 1920px 1165px;*/
+  background-repeat: no-repeat;
+  background-position: top;
+  background-attachment:fixed;
+  color: #222;
+  font-family: 'Open Sans', sans-serif;
+  line-height:1.4em;
+  overflow:auto;
+  font-size: 1.3em;
+  margin:0px;
+  padding:0px;
+}
+
+body:before {
+  content: "";
+  position: relative;
+  top: 0px;
+  left: 50%;
+  bottom: 0px;
+  transform: translateX(-50%);
+  width: 4px;
+}
+
+canvas {
+  width:99%;
+  height:99%;
+  background-color: transparent;
+  position: fixed;
+  clear:both;
+  z-index: 10;
+}
+
+.centered {
+/*
+position: fixed;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+*/
+  margin:100px auto;
+  z-index: 9999;
+  width:100%;
+  max-width:1485px;
+  height:auto;
+}
+
+img a {
+  border:none;
+}
+
+.logo {
+  width:100%;
+  max-width:1485px;
+  margin:0 auto;
+  border:none;
+}
+
+.button {
+  position:relative;
+  border: 1px solid #000;
+  background: #00111c;
+  background: #3b3b3b;
+  padding: 12.5px 25px;
+  color: #ccc;
+  font-size:25px;
+  text-decoration: none;
+  vertical-align: middle;
+  z-index:9999;
+  min-width:300px;
+}
+
+.button:hover {
+  background: #001f33;
+  color: #fff;
+}
+.button:active {
+  background: #000000;
+}
+
+@media only screen and (max-width: 600px) {
+  .button {
+  font-size:15px;
+  }
+}
+
+body .entries {
+  position:relative;
+  top:10px;
+  width: calc(100% - 80px);
+  max-width: 1000px;
+  margin: auto;
+  left: -5px;
+}
+body .entries .entry {
+  width: calc(50% - 70px);
+  float: left;
+  padding: 20px;
+  clear: both;
+  text-align: right;
+}
+body .entries .entry:not(:first-child) {
+  margin-top: -60px;
+}
+body .entries .entry .title {
+  font-size: 1.4em;
+  margin-bottom: 12px;
+  position: relative;
+  color: #fff;
+}
+body .entries .entry .title:before {
+  content: "";
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  border: 4px solid #fff;
+  background-color: #1d1d1d;
+  border-radius: 100%;
+  top: 50%;
+  transform: translateY(-50%);
+  right: -73px;
+  z-index: 1000;
+}
+body .entries .entry .title.big:before {
+  width: 24px;
+  height: 24px;
+  transform: translate(8px, -50%);
+}
+body .entries .entry .body {
+  color: #aaa;
+}
+body .entries .entry .body p {
+  line-height: 1.4em;
+}
+body .entries .entry:nth-child(2n) {
+  text-align: left;
+  float: right;
+}
+body .entries .entry:nth-child(2n) .title:before {
+  left: -63px;
+}
+body .entries .entry:nth-child(2n) .title.big:before {
+  transform: translate(-8px, -50%);
+}
+
+.grid {
+  width:100%;
+  display: -webkit-flex; /* Safari */
+  display: flex;
+  padding:10px 60px;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.col-quarter {
+  -webkit-flex: 1;  /* Safari 6.1+ */
+  -ms-flex: 1;  /* IE 10 */
+  flex: 1;
+  /*margin:10px 80px;*/
+  flex-wrap: wrap;
+  max-width:380px;
+  align-self: center;
+}
+
+.responsive-img {
+  max-width: 100%;
+  display:block;
+  height: auto;
+  min-width:200px;
+  max-width:380px;
+}
+
+@media only screen and (max-width: 1068px) {
+  .responsive-img {
+    max-width:100%;
+    margin: 0 auto;
+    max-width:230px;
+  }
+
+  .col-quarter {
+  -webkit-flex: 1;  /* Safari 6.1+ */
+  -ms-flex: 1;  /* IE 10 */
+  flex: 1;
+  }
+
+}
+
+@media only screen and (max-width: 900px) {
+  .responsive-img {
+    max-width:380px;
+    margin: 0 auto;
+  }
+
+  .col-quarter {
+  max-width:380px;
+  }
+}
+
+#footer {
+  display:block;
+  position:relative;
+  z-index:9999;
+  background: #111;
+  text-align:center;
+  margin:0px;
+  color: #aaa;
+  font-size:16px;
+  background: -moz-linear-gradient(top, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(top, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 100%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to bottom, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#000000', endColorstr='#00000000',GradientType=0 ); /* IE6-9*/
+}
+  </style>
+
+  <script type="application/ld+json" async>
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name" : "BackTrack Linux",
+  "url": "https://www.backtrack-linux.org/",
+  "logo": "https://www.backtrack-linux.org/images/backtrack-black.png"
+}
+  </script>
+
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+
+<canvas id="nokey">
+  Your Browser Doesn't Support Canvas. Please Download FireFox or Chrome or a compatible browser.
+</canvas>
+
+<div class="centered">
+  <a href="https://www.edit.in.th">
+    Information technology tools for educational in thailand.
+  </a>
+
+  <br />
+
+  <center>
+    <h1>
+      <a class="button" href="https://www.edit.in.th/main/">
+        Educational information tools in Thailand
+      </a>
+    </h1>
+  </center>
+
+  <br />
+</div><!-- end centered -->
+
+<div class="entries">
+  <div class="entry">
+    <div class="title">November 23th 2021</div>
+    <div class="body">
+      <p>Register edit.in.th Domain</p>
+    </div><!-- end body -->
+  </div><!-- end entry -->
+</div><!-- end entries -->
+
+<div style="display:block; text-align:center; margin:100px auto; clear:both; top:100px; position:relative; z-index:9999;">
+
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+</div>
+
+<div id="footer">
+  <br />
+  <br />
+
+  <h2 style="display:none;">Visit some of our other projects</h2>
+
+  <div class="grid">
+    <div class="col-quarter">
+      <a href="https://www.edsup.org/" title="" target="_blank" rel="">
+        EDSup.org
+      </a>
+    </div><!-- end col-quarter -->
+    <div class="col-quarter">
+      <a href="http://8rm.edsup.org/" title="" target="" rel="">
+        8 riew model
+      </a>
+    </div><!-- end col-quarter -->
+    <div class="col-quarter">
+      <a href="http://beehive.edsup.org/" title="" target="_blank" rel="">
+        Beehive
+      </a>
+    </div><!-- end col-quarter -->
+    <div class="col-quarter">
+      <a href="ed30.edsup.org" title="" target="_blank" rel="">
+        ED.inded 30th
+      </a>
+    </div><!-- end col-quarter -->
+    <div class="col-quarter">
+      <a href="http://sukho.edsup.org/" title="" target="_blank" rel="">
+        Sukho Computer
+      </a>
+    </div><!-- end col-quarter -->
+  </div><!-- end grid -->
+
+  <br />
+  <br />
+
+  <p style="color: #aaa; font-weight: 300;">
+    &copy; <a href="http://www.edsup.org/">Edsup.org</a> <span>2014-</span><span id="footerYear"></span>. All rights reserved.
+  </p>
+
+  <br />
+
+  <script>function footerDate(){ var getYear=(new Date().getFullYear()); document.getElementById("footerYear").innerHTML = getYear;}footerDate();</script>
+</div><!-- end footer -->
+
+<script>
+var canvas = document.getElementById('nokey'),
+   can_w = parseInt(canvas.getAttribute('width')),
+   can_h = parseInt(canvas.getAttribute('height')),
+   ctx = canvas.getContext('2d');
+
+//console.log(typeof can_w);
+
+var ball = {
+      x: 0,
+      y: 0,
+      vx: 0,
+      vy: 0,
+      r: 0,
+      alpha: 1,
+      phase: 0
+   },
+   ball_color = {
+       r: 207,
+       g: 255,
+       b: 4
+   },
+   R = 2,
+   balls = [],
+   alpha_f = 0.03,
+   alpha_phase = 0,
+
+// Line
+   link_line_width = 0.8,
+   dis_limit = 260,
+   add_mouse_point = true,
+   mouse_in = false,
+   mouse_ball = {
+      x: 0,
+      y: 0,
+      vx: 0,
+      vy: 0,
+      r: 0,
+      type: 'mouse'
+   };
+
+// Random speed
+function getRandomSpeed(pos){
+    var  min = -1,
+       max = 1;
+    switch(pos){
+        case 'top':
+            return [randomNumFrom(min, max), randomNumFrom(0.1, max)];
+            break;
+        case 'right':
+            return [randomNumFrom(min, -0.1), randomNumFrom(min, max)];
+            break;
+        case 'bottom':
+            return [randomNumFrom(min, max), randomNumFrom(min, -0.1)];
+            break;
+        case 'left':
+            return [randomNumFrom(0.1, max), randomNumFrom(min, max)];
+            break;
+        default:
+            return;
+            break;
+    }
+}
+
+function randomArrayItem(arr){
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function randomNumFrom(min, max){
+    return Math.random()*(max - min) + min;
+}
+//console.log(randomNumFrom(0, 10));
+
+// Random Ball
+function getRandomBall(){
+    var pos = randomArrayItem(['top', 'right', 'bottom', 'left']);
+    switch(pos){
+        case 'top':
+            return {
+                x: randomSidePos(can_w),
+                y: -R,
+                vx: getRandomSpeed('top')[0],
+                vy: getRandomSpeed('top')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10)
+            }
+            break;
+        case 'right':
+            return {
+                x: can_w + R,
+                y: randomSidePos(can_h),
+                vx: getRandomSpeed('right')[0],
+                vy: getRandomSpeed('right')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10)
+            }
+            break;
+        case 'bottom':
+            return {
+                x: randomSidePos(can_w),
+                y: can_h + R,
+                vx: getRandomSpeed('bottom')[0],
+                vy: getRandomSpeed('bottom')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10)
+            }
+            break;
+        case 'left':
+            return {
+                x: -R,
+                y: randomSidePos(can_h),
+                vx: getRandomSpeed('left')[0],
+                vy: getRandomSpeed('left')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10)
+            }
+            break;
+    }
+}
+
+function randomSidePos(length){
+    return Math.ceil(Math.random() * length);
+}
+
+// Draw Ball
+function renderBalls(){
+    Array.prototype.forEach.call(balls, function(b){
+       if(!b.hasOwnProperty('type')){
+           ctx.fillStyle = 'rgba('+ball_color.r+','+ball_color.g+','+ball_color.b+','+b.alpha+')';
+           ctx.beginPath();
+           ctx.arc(b.x, b.y, R, 0, Math.PI*2, true);
+           ctx.closePath();
+           ctx.fill();
+       }
+    });
+}
+
+// Update balls
+function updateBalls(){
+    var new_balls = [];
+    Array.prototype.forEach.call(balls, function(b){
+        b.x += b.vx;
+        b.y += b.vy;
+
+        if(b.x > -(50) && b.x < (can_w+50) && b.y > -(50) && b.y < (can_h+50)){
+           new_balls.push(b);
+        }
+
+        // alpha change
+        b.phase += alpha_f;
+        b.alpha = Math.abs(Math.cos(b.phase));
+        //console.log(b.alpha);
+    });
+
+    balls = new_balls.slice(0);
+}
+
+// Loop alpha
+function loopAlphaInf(){
+}
+
+// Draw lines
+function renderLines(){
+    var fraction, alpha;
+    for (var i = 0; i < balls.length; i++) {
+        for (var j = i + 1; j < balls.length; j++) {
+
+           fraction = getDisOf(balls[i], balls[j]) / dis_limit;
+
+           if(fraction < 1){
+               alpha = (1 - fraction).toString();
+
+               ctx.strokeStyle = 'rgba(150,150,150,'+alpha+')';
+               ctx.lineWidth = link_line_width;
+
+               ctx.beginPath();
+               ctx.moveTo(balls[i].x, balls[i].y);
+               ctx.lineTo(balls[j].x, balls[j].y);
+               ctx.stroke();
+               ctx.closePath();
+           }
+        }
+    }
+}
+
+// Calculate distance between two points
+function getDisOf(b1, b2){
+    var  delta_x = Math.abs(b1.x - b2.x),
+       delta_y = Math.abs(b1.y - b2.y);
+
+    return Math.sqrt(delta_x*delta_x + delta_y*delta_y);
+}
+
+// Add balls if there a little balls
+function addBallIfy(){
+    if(balls.length < 20){
+        balls.push(getRandomBall());
+    }
+}
+
+// Render
+function render(){
+    ctx.clearRect(0, 0, can_w, can_h);
+
+    renderBalls();
+
+    renderLines();
+
+    updateBalls();
+
+    addBallIfy();
+
+    window.requestAnimationFrame(render);
+}
+
+// Init Balls
+function initBalls(num){
+    for(var i = 1; i <= num; i++){
+        balls.push({
+            x: randomSidePos(can_w),
+            y: randomSidePos(can_h),
+            vx: getRandomSpeed('top')[0],
+            vy: getRandomSpeed('top')[1],
+            r: R,
+            alpha: 1,
+            phase: randomNumFrom(0, 10)
+        });
+    }
+}
+
+// Init Canvas
+function initCanvas(){
+    canvas.setAttribute('width', window.innerWidth);
+    canvas.setAttribute('height', window.innerHeight);
+
+    can_w = parseInt(canvas.getAttribute('width'));
+    can_h = parseInt(canvas.getAttribute('height'));
+}
+
+window.addEventListener('resize', function(e){
+    //console.log('Window Resize...');
+    initCanvas();
+});
+
+function goMovie(){
+    initCanvas();
+    initBalls(20);
+    window.requestAnimationFrame(render);
+}
+goMovie();
+
+// Mouse effect
+canvas.addEventListener('mouseenter', function(){
+    //console.log('mouseenter');
+    mouse_in = true;
+    balls.push(mouse_ball);
+});
+
+canvas.addEventListener('mouseleave', function(){
+    //console.log('mouseleave');
+    mouse_in = false;
+    var new_balls = [];
+    Array.prototype.forEach.call(balls, function(b){
+        if(!b.hasOwnProperty('type')){
+            new_balls.push(b);
+        }
+    });
+    balls = new_balls.slice(0);
+});
+
+canvas.addEventListener('mousemove', function(e){
+    var e = e || window.event;
+    mouse_ball.x = e.pageX;
+    mouse_ball.y = e.pageY;
+    //console.log(mouse_ball);
+});
+</script>
+
+</body>
+</html>
